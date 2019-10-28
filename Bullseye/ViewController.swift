123db5292d32753d1156d6a769f2eccc208e45d1
Matt Crowder <mcrowder65@gmail.com>
2019-10-28 16:54:35 -0400

    added outline

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -16,23 +16,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        showAlert(title: "Hello, World!", actionTitle: "Awesome", message: "This is my first app!")
+    }
+
+    func showAlert(title: String, actionTitle: String, message: String? = nil, preferredStyle: UIAlertController.Style = .alert) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
+        let action = UIAlertAction(title: actionTitle, style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
     }
-    
-    @IBAction func knockKnock() {
-        let alert = UIAlertController(title: "Who's there?", message: nil, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Butts", style: .default, handler: nil)
-        
-        alert.addAction(action)
-        
-        present(alert, animated: true, completion: nil)
-    }
-
-
 }
 
