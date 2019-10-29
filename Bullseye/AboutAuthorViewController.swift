1c39dd524c80407d61544c4cfa15096e6cfa6b00
Matt Crowder <mcrowder65@gmail.com>
2019-10-28 20:33:23 -0400

    added headshot

---
+++ Bullseye/AboutAuthorViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutAuthorViewController.swift
+//  Bullseye
+//
+//  Created by Matt Crowder on 10/28/19.
+//  Copyright © 2019 Matt. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+       dismiss(animated: true, completion: nil)
+    }
+
+}
