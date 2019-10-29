7cfd6e98e953530afd5f554f2c5a6b6cc9f90151
Matt Crowder <matt.crowder@appian.com>
2019-10-29 09:23:38 -0400

    added the ugly slider

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -23,6 +23,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         startNewRound()
         resetScore()
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizeable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizeable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SmallButton")
+        let trackRightResizeable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizeable, for: .normal)
     }
     
     @IBAction func startOver() {
